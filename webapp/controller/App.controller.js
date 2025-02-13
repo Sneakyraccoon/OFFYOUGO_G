eaf41342f5ecadbe50e04ca170b74f64b21a3f0b
Sneakyraccoon <zhuriko@gmail.com>
2025-02-13 20:39:00 +0200

    commit8

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,1 +1,11 @@
-WHAT SHOULD BE HERE ???
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("ui5.vacation.controller.App", {
+        onInit: function () {
+            console.log("App Controller initialized.");
+        }
+    });
+});
