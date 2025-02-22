2c3775ecaea67c410948aed48b7b66cf4648852c
Sneakyraccoon <zhuriko@gmail.com>
2025-02-22 11:27:31 +0200

    Commit2

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,32 +1,33 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
-    
-  ], function (UIComponent, JSONModel) {
-    "use strict";
-  
-    return UIComponent.extend("ui5.vacation.Component", {
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel",
+  "mockserver/mockserver" // Подключаем mockserver
+], function (UIComponent, JSONModel, mockserver) {
+  "use strict";
+
+  return UIComponent.extend("ui5.vacation.Component", {
       metadata: {
-        manifest: "json"
+          manifest: "json"
       },
-  
+
       init: function () {
-        // Инициализация MockServer
-// Безопасный запуск MockServer (можно временно закомментировать)
-// if (mockserver && typeof mockserver.init === "function") {
-//   mockserver.init();
-// }
-  
-        // Вызов init родительского компонента
-        UIComponent.prototype.init.apply(this, arguments);
-  
-        // Установка модели пользователя
-        var oUserModel = new JSONModel();
-        this.setModel(oUserModel, "userModel");
-  
-        // Инициализация маршрутизатора
-        this.getRouter().initialize();
+          // Запуск MockServer
+          if (mockserver && typeof mockserver.init === "function") {
+              mockserver.init();
+              console.log("MockServer запущен!");
+          }
+
+          // Вызов init родительского компонента
+          UIComponent.prototype.init.apply(this, arguments);
+
+          // Установка модели пользователя
+          var oUserModel = new JSONModel();
+          this.setModel(oUserModel, "userModel");
+
+          // Инициализация маршрутизатора
+          this.getRouter().initialize();
       }
-    });
   });
+});
+
   
